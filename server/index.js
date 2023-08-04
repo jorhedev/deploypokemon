11f4065ce13332dcc5f645842206c064a20e2d5e
jorhedev <hijorhe@gmail.com>
2023-08-04 15:34:49 -0600

    config server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const  getAllTypes  = require('./src/controllers/getAllTypes.js')
+require('dotenv').config();
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(process.env.PORT, () => {
     getAllTypes();
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log('%s listening at', process.env.PORT); // eslint-disable-line no-console
   });
 });
